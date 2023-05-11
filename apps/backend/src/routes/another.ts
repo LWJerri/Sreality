82945ba50972de1eb4d6dfab815abe217aa35692
LWJerri <loginmanx@gmail.com>
2023-05-12 01:19:16 +0300

    Rename `mainRoute` to `anotherRoute`

--- apps/backend/src/routes/another.ts
+++ apps/backend/src/routes/another.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
-export async function mainRequest(req: FastifyRequest, res: FastifyReply) {
+export async function anotherRequest(req: FastifyRequest, res: FastifyReply) {
   return await res.status(200).send({ time: new Date().toISOString(), host: req.hostname });
 }
